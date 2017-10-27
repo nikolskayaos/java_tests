5179ba6ddd1824c719cb56a4a0db7610e0ed4d7d
NikolskayaOlya <33005850+nikolskayaos@users.noreply.github.com>
2017-10-27 15:33:58 +0300

    д/з 2. добавлен расчет расстояние между точками

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -1,5 +1,21 @@
+import static java.lang.Math.sqrt;
+
 public class FirstProgram{
 	public static void main (String[] args){
-	System.out.println("Hello World!");
+		System.out.println("Расстояние между точками:");
+
+		Point points = new Point(10, 11, 1, 3);
+		System.out.println();
+		System.out.println("Способ 1.");
+		System.out.println("Точки:  p1(" + points.x1 + "," + points.y1 + ")" + "и p2(" + points.x2 + "," + points.y2 + ")");
+		System.out.println("Расстояние между двумя точками p1 и p2 = "  + distance(points));
+		System.out.println();
+		System.out.println("Способ 2.");
+		System.out.println("Точки:  p1(" + points.x1 + "," + points.y1 + ")" + "и p2(" + points.x2 + "," + points.y2 + ")");
+		System.out.println("Расстояние между двумя точками p1 и p2 = "  + points.distance());
+	}
+
+	public static double distance(Point points){
+		return sqrt((points.x1 - points.x2)*(points.x1 - points.x2) + (points.y1 - points.y2)*(points.y1 - points.y2));
 	}
 }
